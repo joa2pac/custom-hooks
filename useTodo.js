7fdea7b1c6f69c70d0bb621d0ab42fe27555cb95
joaquin <joaquinahuel93@gmail.com>
2023-02-04 15:11:30 -0300

    primer commit

---
+++ useTodo.js
@@ -0,0 +1,64 @@
+import { useEffect, useReducer, useState } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const initialState =[
+    // {
+    //     id: new Date().getMinutes(),
+    //     description: 'Recolectar la piedra del alma',
+    //     done: false,
+    // },
+    // {id: new Date().getMinutes() * 3,
+    //     description: 'Recolectar la piedra del infinito',
+    //     done: false,
+    // }
+];
+
+const init = () => {
+    return JSON.parse( localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect (() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos])
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    const handleToggleTodo = ( id ) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id
+        });
+    }
+
+    
+    return {
+        todos, 
+
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo=> !todo.done).length,
+
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+        
+    }
+}
